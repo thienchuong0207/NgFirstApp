88e50e89459014cb27494a4886901d2662a208d3
Chuong Ton <thienchuong0207@gmail.com>
2018-07-24 14:43:05 -0600

    Routers - "Home" Tab & "Class" Tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
 import { ClassComponent } from './component/class/class.component';
 import { StudentFormComponent } from './component/student/form/student.form.component';
 import { ClassService } from './service/ClassService';
 import { LoggingService } from './service/LoggingService';
 
+const appRoutes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "class", component: ClassComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     ClassComponent,
     StudentFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ ClassService, LoggingService ],
   bootstrap: [AppComponent]
